764bf735297e275427586087d95d08b2113f556b
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 14:04:31 -0400

    fetching data

--- client.js
+++ client.js
@@ -1,13 +1,20 @@
 const net = require("net");
 const connect = function () {
-const conn = net.createConnection({
+  const conn = net.createConnection({
     host: "135.23.223.133",
     port: 50542
   });
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected")
+    conn.write("Name:MOE")
+  })
+  conn.on("data", (data) => {
+    console.log(data)
+  })
 
   return conn;
 };
 
 
-  module.exports = connect;
+module.exports = connect;

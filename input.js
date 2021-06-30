5bc9041201fa864446f0bebc28e377cb058fce30
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 15:53:33 -0400

    moving the two func in this new file

---
+++ input.js
@@ -0,0 +1,25 @@
+const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  
+  
+  };
+  
+  
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+  
+
+
+
+  module.exports = {
+      handleUserInput,
+      setupInput
+  }

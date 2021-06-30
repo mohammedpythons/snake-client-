b1689c2060beeda63954663cb9dbdbf5aea1e5a4
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 14:59:27 -0400

    moves commands

--- client.js
+++ client.js
@@ -7,7 +7,13 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected")
-    conn.write("Name:MOE")
+    conn.write("Name: MOE")
+    conn.write("Move: up")
+    conn.write("Move: left")
+    conn.write("Move: up")
+    
+    
+    
   })
   conn.on("data", (data) => {
     console.log(data)

e2067a133e06712ce4d0962946576b17281391d1
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 15:55:07 -0400

    adjusting funtions

--- play.js
+++ play.js
@@ -4,25 +4,7 @@ const connect = require("./client")
 // establishes a connection with the game server
 
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
 
-
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput()
 
 
 

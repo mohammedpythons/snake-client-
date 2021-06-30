18134ff9afda9f1a7211bd9ffb7ed0950cfe869b
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 13:36:22 -0400

    snake game

---
+++ play.js
@@ -0,0 +1,12 @@
+
+const connect = require("./client")
+// establishes a connection with the game server
+
+
+  // interpret incoming data as text
+
+
+
+
+console.log("Connecting ...");
+connect();
